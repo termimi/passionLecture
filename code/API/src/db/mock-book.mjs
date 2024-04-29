8f6255bd7cf98a5cee25043132d4d3a1e0c4a189
termimi <123395118+termimi@users.noreply.github.com>
2024-04-29 15:13:39 +0200

    API finis

---
+++ code/API/src/db/mock-book.mjs
@@ -0,0 +1,78 @@
+let dataBooks = [
+    {
+        id: 1,
+        price: 9.99,
+        created: new Date(),
+        title: "A Christmas Carol",
+        image: "https://th.bing.com/th/id/OIP.Z5KLQbybGAxlM5JdjOWeWwAAAA?rs=1&pid=ImgDetMain",
+        categories_id: 1,
+        customers_id:1,
+        authors_id:1,
+        page_count: 328,
+        summary: "Dans un monde totalitaire, Winston Smith tente de s'élever contre le contrôle oppressif du Parti."
+    },
+    {
+        id: 2,
+        price: 14.99,
+        created: new Date(),
+        title: "Oliver twist",
+        image: "https://media.senscritique.com/media/000006844185/source_big/Le_Seigneur_des_Anneaux_La_Communaute_de_l_anneau.jpg",
+        categories_id: 2,
+        customers_id:1,
+        authors_id:2,
+        page_count: 423,
+        summary: "Frodon Sacquet entreprend un voyage périlleux pour détruire un anneau maléfique et sauver la Terre du Milieu."
+    },
+    {
+        id: 3,
+        price: 12.99,
+        created: new Date(),
+        title: "Sherlock Holmes",
+        image: "https://th.bing.com/th/id/OIP.TY4VTNCbLsWleOyoWQVA9QAAAA?rs=1&pid=ImgDetMain",
+        categories_id: 2,
+        customers_id:1,
+        authors_id:3,
+        page_count: 320,
+        summary: "Harry Potter découvre qu'il est un sorcier et commence son apprentissage à Poudlard, une école de magie."
+    },
+    {
+        id: 4,
+        price: 8.49,
+        created: new Date(),
+        title: " Les trois mousquetaires",
+        image: "https://th.bing.com/th/id/R.780479fec68a1ed711e3a0355596e900?rik=R01DJ%2bHJ4NzgFQ&pid=ImgRaw&r=0",
+        categories_id: 3,
+        customers_id:4,
+        authors_id:4,
+        page_count: 384,
+        summary: "L'histoire de l'orgueilleuse Elizabeth Bennet et du hautain Mr. Darcy dans l'Angleterre du XIXe siècle."
+    },
+    {
+        id: 5,
+        price: 6.99,
+        created: new Date(),
+        title: "Fables",
+        image: "https://media.s-bol.com/qwE49mk1MV7/796x1200.jpg",
+        categories_id: 4,
+        customers_id:5,
+        authors_id:5,
+        page_count: 96,
+        summary: "Un conte philosophique sur l'amitié et l'amour, racontant les aventures d'un petit prince venu d'une autre planète."
+    },
+    {
+        id: 6,
+        price: 19.99,
+        created: new Date(),
+        title: " Le tour du monde en quatre-vingts jours",
+        image: "https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781451686005_9781451686005_hr.jpg",
+        categories_id: 5,
+        customers_id:6,
+        authors_id:6,
+        page_count: 1488,
+        summary: "L'histoire de Jean Valjean, un ancien bagnard, et de son parcours pour la rédemption dans la France du XIXe siècle."
+    }
+]
+
+
+
+export { dataBooks };
